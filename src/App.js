faec06bf9e66e4430d2e5c88e412071ce4e59c32
saint emeka <saintobinna62@gmail.com>
2024-01-17 23:31:21 +0100

    react-checkpoint

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Checkpoint from "./Components/Checkpoint";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Checkpoint />
+    </div>
+  );
+};
+
+export default Checkpoint;
